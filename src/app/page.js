7a826c9830af3a4fb0ec9f8ed42e0da9e1829a89
majidbelbasi <majidbelbasi57@gmail.com>
2024-11-09 12:26:23 +0330

    add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import HomeHeader from "@/components/HomeHeader";
 
 export default function Home() {
-  return <h1>Home</h1>
+  return <>
+    <HomeHeader />
+  </>
 
 }
     
